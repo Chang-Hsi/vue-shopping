4c46e47d25a380bc3f67825713dd7e44e8ebfdb9
Chang-Hsi <watasiwa8531@gmail.com>
2025-12-25 14:05:39 +0800

    chore: deploy to github pages

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
     vueDevTools(),
     tailwindcss(),
   ],
+  base: "/vue-shopping/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
